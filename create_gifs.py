d904794bf3860aeb822ae2a54a96576f3650dd19
spark71 <noxdrummer@gmail.com>
2024-07-11 22:05:30 +0700

    upd

--- create_gifs.py
+++ create_gifs.py
@@ -19,7 +19,7 @@ def create_gifs_from_video(video_path, fragments, output_prefix="output", fps=10
 
     for i, (start, end) in tqdm(enumerate(fragments)):
         # output_path = f"{output_prefix}_{i+1}.gif"
-        output_path = f"gif/{output_prefix}_{i+1}.gif"
+        output_path = f"gif/{output_prefix}_{i + 1}.gif-files"
         cmd = [
             "ffmpeg",
             "-ss", start,  # Формат hh:mm:ss
@@ -36,11 +36,7 @@ def create_gifs_from_video(video_path, fragments, output_prefix="output", fps=10
 
 video_path = ""
 fragments = [
-    ("00:01:25", "00:01:40"),  # Первый фрагмент: с 0 до 5 секунды
-    ("00:03:20", "00:03:35"),  # Второй фрагмент: с 10 до 15 секунды
-    ("00:04:10", "00:04:30"),  # Третий фрагмент: с 20 до 25 секунды
-    ("00:05:20", "00:05:40"),  # Третий фрагмент: с 20 до 25 секунды
-    ("00:06:30", "00:07:00"),  # Третий фрагмент: с 20 до 25 секунды
-    ("00:08:05", "00:08:30"),  # Третий фрагмент: с 20 до 25 секунды
+    # ("00:01:25", "00:01:40"),
+    # ("00:03:20", "00:03:35"),
 ]
 create_gifs_from_video(video_path, fragments)
